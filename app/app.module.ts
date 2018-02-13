0b042a438a5efc55c8b29b577fda94eb824fc4d2
Alexei Popkov <reystleen@gmail.com>
2018-02-13 20:14:26 +0100

    Buttons

--- app/app.module.ts
+++ app/app.module.ts
@@ -16,6 +16,7 @@ import { DrawerComponent } from "./shared/drawer/drawer.component";
 import { DishService } from "./services/dish.service";
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { FavoriteService } from './services/favorite.service';
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 
 import { baseURL } from './shared/baseurl';
@@ -53,6 +54,7 @@ import { baseURL } from './shared/baseurl';
         DishService,
         PromotionService,
         LeaderService,
+        FavoriteService,
         ProcessHTTPMsgService
     ],
     schemas: [
